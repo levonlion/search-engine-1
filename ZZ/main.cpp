2cd3a28762cbe807fe2ed1daabb6590a8a5ebcf2
Zhora <z.zhora.gasparyan@icloud.com>
2021-02-24 13:32:02 +0400

    1

--- ZZ/main.cpp
+++ ZZ/main.cpp
@@ -1,14 +1,6 @@
-//
-//  main.cpp
-//  ZZ
-//
-//  Created by Zhora Gasparyan on 2/24/21.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+    std::cout << "Hello";
 }
+
