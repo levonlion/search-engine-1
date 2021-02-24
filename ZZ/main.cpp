3df78f89c149ef9d20eb577018ee248087efd3ef
Zhora <z.zhora.gasparyan@icloud.com>
2021-02-24 13:37:24 +0400

    Initial Commit

---
+++ ZZ/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ZZ
+//
+//  Created by Zhora Gasparyan on 2/24/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
