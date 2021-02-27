f701d878b5f99cb3dd8cb3aa71432705acc4589f
Zhora <z.zhora.gasparyan@icloud.com>
2021-02-27 11:39:43 +0400

    Init

--- main.cpp
+++ main.cpp
@@ -4,26 +4,45 @@
 #include <stdio.h>
 #include <curl/curl.h>
 
+size_t CurlWrite_CallbackFunc_StdString(void *contents, size_t size, size_t nmemb, std::string *s) {
+    size_t newLength = size*nmemb;
+    try
+    {
+        s->append((char*)contents, newLength);
+    }
+    catch(std::bad_alloc &e)
+    {
+        //handle memory problem
+        return 0;
+    }
+    return newLength;
+}
+
 int main(void) {
     CURL *curl;
     CURLcode res;
     
     curl = curl_easy_init();
+    std::string s;
     if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
-        /* example.com is redirected, so we tell libcurl to follow redirection */
+        curl_easy_setopt(curl, CURLOPT_URL, "curl.haxx.se");
         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, CurlWrite_CallbackFunc_StdString);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
         
         /* Perform the request, res will get the return code */
         res = curl_easy_perform(curl);
         /* Check for errors */
-        if(res != CURLE_OK)
-            fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                    curl_easy_strerror(res));
+        if(res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        }
         
         /* always cleanup */
         curl_easy_cleanup(curl);
     }
+    
+    std::cout << s << std::endl;
     return 0;
 }
 
