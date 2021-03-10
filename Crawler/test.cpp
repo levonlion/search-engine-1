e8f1584bc47b031b40840f2730817d67ac1d7f37
Zhora <z.zhora.gasparyan@icloud.com>
2021-03-10 12:20:15 +0400

    Parsing from html all urls

---
+++ Crawler/test.cpp
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include <gumbo.h>
+#include "PageLoader/PageLoader.hpp"
+#include "Parser/Parser.hpp"
+
+int main() {
+    std::string url = "https://rau.am";
+    PageLoader pL;
+    Page res = pL.load(url);
+    
+    Parser parser(url, res.getBody());
+    
+    parser.parse();
+
+    for (const std::string& u : parser.getUrls()) {
+        std::cout << u << std::endl;
+    }
+    
+}
