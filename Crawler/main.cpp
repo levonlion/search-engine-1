3878d5caacf05e7bc8e3f30a0da55d9f417b0001
Zhora <z.zhora.gasparyan@icloud.com>
2021-04-14 09:15:13 +0400

    Crawler algorithm implementation with tests.

--- Crawler/main.cpp
+++ Crawler/main.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
+#include <ctime>
 #include "WebsiteStore/WebsiteStore.hpp"
 #include "LinkStore/LinkStore.hpp"
 #include "PageLoader/PageLoader.hpp"
 #include "Parser/Parser.hpp"
+#include "DocumentStore/DocumentStore.hpp"
 
 int main() {
     WebsiteStore websiteStore;
-//    websiteStore.add(Website("rau.am", "https://rau.am", 0));
-    websiteStore.add(Website("macrumors.com", "https://www.macrumors.com", 0));
+    websiteStore.add(Website("rau.am", "https://rau.am", 0));
+//    websiteStore.add(Website("macrumors.com", "https://www.macrumors.com", 0));
+//    websiteStore.add(Website("google.com", "https://translate.google.com", 0));
     const auto& websites = websiteStore.getAll();
     
     LinkStore linkStore;
     PageLoader pageLoader;
+    DocumentStore documentStore;
     for (const auto& website : websites) {
         
-//        if (website.getLastCrawlingTime() + someUpdateInterval > time(NUll)) {
-//            continue;
-//        }
-        
+        if (website.getLastCrawlingTime() + 60 > time(NULL)) {
+            continue;
+        }
+
         const auto& homepageLink = linkStore.getBy(website.getHomepage());
         
         if (homepageLink.has_value()) {
@@ -35,7 +39,7 @@ int main() {
             
             for (const auto& link : links) {
                 const auto& page = pageLoader.load(link.getUrl());
-                if (page.isError() || page.getStatus() < 200 ||  page.getStatus() >= 300) {
+                if (page.isError() || page.getStatus() < 200 || page.getStatus() >= 300) {
                     linkStore.update(Link(link.getUrl(), link.getDomain(), LinkStatus::ERROR, time(NULL)));
                     continue;
                 }
@@ -44,22 +48,37 @@ int main() {
                 
                 parser.parse();
                 
-                for (const std::string& u : parser.getUrls()) {
-                    std::cout << u << std::endl;
-                }
+//                for (const std::string& u : parser.getUrls()) {
+//                    std::cout << u << std::endl;
+//                }
                 
+                // FIXME: Delete these printing ⬇️ (for testing textExtractor, titleExtractor, ...)
+//                std::cout << page.getUrl() << std::endl;
+//                std::cout << parser.getDescription() << std::endl;
+//                std::cout << parser.getTitle() << std::endl;
+//                std::cout << parser.getAllText() << std::endl;
+//                std::cout << "\n----------------------------------------\n" << std::endl;
+
                 // create document from parser and save
+                documentStore.save(Document(page.getUrl(), parser.getTitle(), parser.getDescription(), parser.getAllText()));
+                
+                // FIXME: Delete these printing ⬇️ (viewing documents)
+                for (const auto& elem : documentStore.getAll()) {
+                    std::cout << elem.getUrl() << std::endl;
+                }
+                std::cout << "\n----------------------------------------\n" << std::endl;
                 
                 for (const auto& newUrl : parser.getUrls()) {
                     if (linkStore.getBy(newUrl).has_value()) {
                         continue;
                     }
-                    linkStore.update(Link(newUrl, website.getDomain(), LinkStatus::WAITING, time(NULL)));
+                    linkStore.add(Link(newUrl, website.getDomain(), LinkStatus::WAITING, time(NULL)));
                 }
                 
                 linkStore.update(Link(link.getUrl(), link.getDomain(), LinkStatus::LOADED, time(NULL)));
             }
         }
+        
+        websiteStore.update(Website(website.getDomain(), website.getHomepage(), time(NULL)));
     }
 }
-
